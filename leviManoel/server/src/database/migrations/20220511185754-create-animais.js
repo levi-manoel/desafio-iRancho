116290d13cb7532adc0aa025ba1a5ef7fece1a77
levi-manoel <levimanoel.deob@gmail.com>
2022-05-11 16:08:06 -0300

    impl: Animal model and migration

---
+++ leviManoel/server/src/database/migrations/20220511185754-create-animais.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+      await queryInterface.createTable('animais', { 
+      id:{
+        autoIncrement:true,
+        primaryKey:true,
+        type: DataTypes.BIGINT,
+      },
+      fk_id_pessoa:{
+        references:{
+          model:'pessoas',
+          key:'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull:false,
+        type:DataTypes.BIGINT,
+      },
+      id_fazenda:{
+        allowNull:false,
+        type: DataTypes.BIGINT,
+      },
+      no_animal:{
+        allowNull:false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      no_raca:{
+        allowNull:false,
+        type: DataTypes.STRING,
+      },
+      sexo:{
+        allowNull:false,
+        type: DataTypes.ENUM('M','F'),
+      },
+      vr_peso:{
+        allowNull:false,
+        type: DataTypes.FLOAT,
+      },
+      dt_nascimento:{
+        allowNull: false,
+        type: DataTypes.DATEONLY,
+      },
+    });
+  },
+
+  down:  async (queryInterface)=> {
+       await queryInterface.dropTable('animais');
+
+  }
+};
---
+++ leviManoel/server/src/database/models/Animal.js
@@ -0,0 +1,63 @@
+import { Sequelize, DataTypes, Model, BIGINT } from "sequelize";
+const options = require("./../../config/database.js");
+const sequelize = new Sequelize(options);
+
+export class Animal extends Model {
+  static associate(models){
+    Animal.belongsTo(models.Pessoa, {
+      foreignKey: 'fk_id_pessoa',
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+
+    Animal.belongsToMany(models.Lote, {
+      through: 'animais_lote',
+    });
+  }
+};
+
+Animal.init({
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  fk_id_pessoa: {
+    type: DataTypes.BIGINT,
+    references: {
+      model: {
+        tableName: 'pessoas',
+      },
+      key: 'id',
+    }
+  },
+  id_fazenda: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+  no_animal: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  no_raca: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sexo: {
+    type: DataTypes.ENUM('M', 'F'),
+    allowNull: false,
+  },
+  vr_peso: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  dt_nascimento: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'animais',
+  modelName: 'Animal',
+});
