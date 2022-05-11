e0d2d992d324425931d21597796c04277282cca4
levi-manoel <levimanoel.deob@gmail.com>
2022-05-11 11:12:12 -0300

    impl: Pessoa model and migration

---
+++ leviManoel/server/src/database/migrations/20220511140126-create-pessoas.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable('Pessoas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      no_pessoa: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      no_email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      endereco: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      sexo: {
+        allowNull: false,
+        type: DataTypes.ENUM('M', 'F'),
+      },
+      ic_ativo: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Pessoas');
+  }
+};
