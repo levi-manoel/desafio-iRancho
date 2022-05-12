6ee4c74e717a4c5fb781509a0883a8453ce038f4
levi-manoel <levimanoel.deob@gmail.com>
2022-05-12 18:36:39 -0300

    impl: GET /pessoas working"

--- leviManoel/server/src/app.js
+++ leviManoel/server/src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
-// routes
 import pessoasRouter from './routes/pessoas.router.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/pessoas', pessoasRouter);
 
 app.listen(3001, () => {
   console.log('app listening on port 3001');
