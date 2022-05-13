aeabd126b0268f7499ee0b1064be265111fc9476
levi-manoel <levimanoel.deob@gmail.com>
2022-05-13 09:50:03 -0300

    impl: POST /pessoas validations

---
+++ leviManoel/server/src/middlewares/postPessoa.validations.js
@@ -0,0 +1,57 @@
+function validateName(name) {
+  if (!name) return { code: 400, response: { error: 'Nome não pode ser vazio' }};
+
+  return true;
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  
+  if (!email) return { code: 400, response: { error: 'Email não pode ser vazio' }};
+  if (!emailRegex.test(email)) return { code: 400, response: { error: 'Email em formato inválido' }};
+
+  return true;
+}
+
+function validateAddress(address) {
+  if (!address) return { code: 400, response: { error: 'Endereço não pode ser vazio' }};
+
+  return true;
+}
+
+function validateGender(gender) {
+  if (!gender) return { code: 400, response: { error: 'Sexo não pode ser vazio' }};
+  if (gender !== 'M' && gender !== 'F') return { code: 400, response: { error: 'Sexo inválido' }};
+
+  return true;
+}
+
+function validateIcAtivo(icAtivo) {
+  if (!icAtivo) return { code: 400, response: { error: 'ic_ativo não pode ser vazio' }};
+  if (icAtivo !== true && icAtivo !== false) return { code: 400, response: { error: 'ic_ativo inválido' }};
+
+  return true;
+}
+
+function validatePostPessoa(req, res, next) {
+  const { no_pessoa, no_email, endereco, sexo, ic_ativo } = req.body;
+
+  const isNameValid = validateName(no_pessoa);
+  if (isNameValid !== true) return res.status(isNameValid.code).json(isNameValid.response);
+
+  const isEmailValid = validateEmail(no_email);
+  if (isEmailValid !== true) return res.status(isEmailValid.code).json(isEmailValid.response);
+
+  const isAddressValid = validateAddress(endereco);
+  if (isAddressValid !== true) return res.status(isAddressValid.code).json(isAddressValid.response);
+
+  const isGenderValid = validateGender(sexo);
+  if (isGenderValid !== true) return res.status(isGenderValid.code).json(isGenderValid.response);
+
+  const isIcAtivoValid = validateIcAtivo(ic_ativo);
+  if (isIcAtivoValid !== true) return res.status(isIcAtivoValid.code).json(isIcAtivoValid.response);
+
+  next();
+}
+
+export default validatePostPessoa;
--- leviManoel/server/src/routes/pessoas.router.js
+++ leviManoel/server/src/routes/pessoas.router.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import pessoasControllers from '../controllers/pessoas.controllers.js';
 
+import validatePostPessoa from '../middlewares/postPessoa.validations.js';
+
 const router = Router();
 
 router
   .get('/', pessoasControllers.getAll)
   .get('/:id', pessoasControllers.getById)
-  .post('/', pessoasControllers.create);
+  .post('/', validatePostPessoa, pessoasControllers.create);
 
 export default router;
