c04db680e8d2212d88af1c1b2756423cecfb68e7
levi-manoel <levimanoel.deob@gmail.com>
2022-05-14 11:54:30 -0300

    basic router

---
+++ leviManoel/client/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <h1>Fazenda 3000</h1>
+    <h3>Fazendo acontecer</h3>
+
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/sobre">Sobre</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- leviManoel/client/src/router/index.js
+++ leviManoel/client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/sobre',
+      name: 'sobre',
+      component: About
     }
   ]
 })
