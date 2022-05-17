e7e9fa0dbd4a66eb2d3f80e5bd3584f8ffbba4a1
levi-manoel <levimanoel.deob@gmail.com>
2022-05-17 18:53:14 -0300

    remote database

--- projeto/server/src/config/database.cjs
+++ projeto/server/src/config/database.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  username: 'root',
-  password: '123456',
-  database: 'desafio_iRancho',
-  host: 'localhost',
-  port: '3002',
+  username: 'sql3493115',
+  password: 'VHf6nY616i',
+  database: 'sql3493115',
+  host: 'sql3.freemysqlhosting.net',
+  port: '3306',
   dialect: 'mysql',
 }
